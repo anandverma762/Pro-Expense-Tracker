88824c399429aaf231911c28db0b4cc8ccd368c9
anandverma762@gmail.com <anandverma762@gmail.com>
2023-06-05 21:09:01 +0530

    Password encrypted

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -1,11 +1,18 @@
 const User = require('../Models/user');
+const bcrypt = require('bcrypt');
 
 exports.signUpUser = (req, res, next) => {
   const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
   User.findOne({ where: { email: email } })
     .then((user) => {
       if (!user) {
-        User.create(req.body)
+        const saltRounds = 10;
+        bcrypt.hash(password ,saltRounds,(err,result)=>{
+
+        
+        User.create({name:name,email:email,password:result})
           .then(() => {
             res.status(201).json({ message: 'User created successfully' });
           })
@@ -13,6 +20,8 @@ exports.signUpUser = (req, res, next) => {
             console.log(err);
             res.status(500).json({ message: 'Internal Server Error' });
           });
+
+        })
         
         } else {
         res.status(409).json({ message: 'User already exists' });
@@ -34,13 +43,15 @@ exports.loginUser = (req, res, next) => {
   User.findOne({ where: { email: email } })
     .then((user) => {
       if (!user) {
-        res.status(401).json({ message: 'Invalid email or password' });
+        res.status(404).json({ message: 'Invalid email or password' });
       } else {
-        if (user.password === password) {
+        bcrypt.compare(password ,user.password,(err,result)=>{
+        if (result === true) {
           res.status(200).json({ message: 'Login successful' });
         } else {
-          res.status(401).json({ message: 'Invalid email or password' });
+          res.status(401).json({ message: 'User not authorized' });
         }
+      })
       }
     })
     .catch((err) => {
